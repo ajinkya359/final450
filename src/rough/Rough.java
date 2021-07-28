919073a8eec5716793043dfbff92030f3631048a
Ajinkya Shinde <sajinkya359@gmail.com>
2021-07-28 17:28:37 +0530

    ass hole

--- src/rough/Rough.java
+++ src/rough/Rough.java
@@ -1,10 +1,7 @@
 package rough;
 
-
-
-public class Rough {
-	public static void main(String[] args) {
-		System.out.print("Hello");
-
+public class Rough{
+	public static void main(String[] args){
+		System.out.println("HEllo world");
 	}
 }
